64917d33c14f02aa52ea3ca23a13d02b58b0e100
User <User@WIN-7UC320QEUGG>
2023-05-03 12:07:35 +0300

    Добавьте файлы проекта.

---
+++ SerializationC/Extension.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace GenericsExtension
+{
+    public static class JsonExtension
+    {
+        public static string ToJson<T>(this T obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+    }
+}
---
+++ SerializationC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GenericsExtension;
+
+namespace SerializationC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new { Name = "Dmitriy", Age = 18 };
+            string jString = obj.ToJson();
+            Console.WriteLine(jString);
+            Console.ReadKey();
+        }
+    }
+}
